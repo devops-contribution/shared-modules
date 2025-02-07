6bfa7e9d0d01f569d412612fda9aaa84fb2aa111
Mukesh <mukesh@EMPID24089.local>
2025-02-07 10:24:30 +0530

    update

--- modules/node-group/main.tf
+++ modules/node-group/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_node_group" "node_group" {
   cluster_name    = var.cluster_name
   node_group_name = var.node_group_name
-  node_role_arn   = var.node_role_arn
+  node_role_arn   = aws_iam_role.eks_node_role.arn
   subnet_ids      = var.subnet_ids
   instance_types  = var.instance_types
 
@@ -15,3 +15,34 @@ resource "aws_eks_node_group" "node_group" {
   }
 }
 
+
+# This defines the IAM role ARN that provides permissions for the EC2 instances (nodes) in the EKS worker node group.
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ec2_container_registry" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
