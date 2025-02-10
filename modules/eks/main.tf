782e5bfe3476f821062493f56e4bdaae4ea4c1d7
Mukesh <mukesh@EMPID24089.local>
2025-02-10 09:24:46 +0530

    update

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -43,3 +43,39 @@ resource "aws_eks_node_group" "node-grp" {
     max_unavailable = 1
   }
 }
+
+
+# Deploy Argocd 
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = var.name
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = var.name
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = var.version
+
+  values = [
+    <<EOF
+server:
+  service:
+    type: LoadBalancer
+EOF
+  ]
+}
+~                    
