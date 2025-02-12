d0aa756ac3e29282f8b5ffd7c5f5451c4e26b509
Mukesh <mukesh@EMPID24089.local>
2025-02-12 10:15:15 +0530

    update

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -34,8 +34,8 @@ resource "aws_eks_node_group" "node-grp" {
   }
 
   scaling_config {
-    desired_size = 2
-    max_size     = 2
+    desired_size = 3
+    max_size     = 3
     min_size     = 1
   }
 
