db659e358b5ca71bf88364c3b9fe3c7000cb4ab9
Mukesh <mukesh@EMPID24089.local>
2025-02-18 09:30:31 +0530

    update

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,14 +2,18 @@ output "vpc_id" {
   value = aws_vpc.eks_vpc.id
 }
 
-output "public_subnet_az1_id" {
+output "private_subnet_az1_id" {
   value = aws_subnet.public_subnet_az1.id
 }
 
-output "public_subnet_az2_id" {
+output "private_subnet_az2_id" {
   value = aws_subnet.public_subnet_az2.id
 }
 
 output "internet_gateway" {
   value = aws_internet_gateway.eks_internet_gateway.id
+}
+
+output "nat_gateway" {
+  value = 
 }
