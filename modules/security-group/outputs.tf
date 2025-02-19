095a11fb6f3ba60b9b69d0540d8bc8439b93fedb
Mukesh <mukesh@EMPID24089.local>
2025-02-19 18:19:50 +0530

    update

--- modules/security-group/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "eks_security_group_id" {
-  value = aws_security_group.eks_security_group.id
-}
-
-
-output "vpc_link_sg_id" {
-  value = aws_security_group.allow_all_traffic.id
-}
--- modules/security-group/variables.tf
+++ modules/security-group/variables.tf
@@ -1,7 +1,3 @@
-variable "vpc_id" {
-  type = string
-}
 
-variable "vpc_cidr" {
-  type = string
-}
+
+
